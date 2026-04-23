c144c7bcf3edd83407df8f4c19f11d15eccb0256
Joe Osorio <joe.osorio.oso@gmail.com>
2026-04-23 15:05:16 -0700

    P8, jala pero al reves

--- 2_Laboratorio/L08_Tema/src/main.asm
+++ 2_Laboratorio/L08_Tema/src/main.asm
@@ -3,10 +3,13 @@
 %include "my_routines.asm"
 
 section .data
-    paso0: db "Laboratorio",10,0
-    msjP1_0:   db "Capturar Cadena",10,0
-    msjP1_1:   db  "Ingrese cadena: ", 0
+    paso0:      db "Laboratorio",10,0
+    msjP1_0:    db "Capturar Cadena binaria",10,0
+    msjP1_0_1:  db "Ejemplo: 0110111100011000",10,0
+    msjP1_1:    db "Ingrese cadena binaria: ", 0
     msjP1_2:    db "La cadena es: ", 0
+    StrBin:  db "0110111100011000",0
+    len: equ $-StrBin
 
 section .bss
     cadena          resb    254
@@ -15,6 +18,7 @@ section .bss
     simbolos        resb    100 ; Todavia no nos dice.
     palabras        resb    4   ; indica cuantas palabas tiene.
     temp            resb    8
+    cad             resb    4
 
 section .text
     global _start
@@ -27,36 +31,90 @@ _start:
     call    new_puts
     mov     edx,    msjP1_0
     call    new_puts
-    mov     edx,    msjP1_1
-    call    new_puts
-    mov     ecx, 254
-    mov     edx, cadena
-    mov     ecx, 254
-    call    inputStr
+    ; mov     edx,    msjP1_1
+    ; call    new_puts
+    ; mov     ecx, 254
+    ; mov     edx, cadena
+    ; mov     ecx, 254
+    ; call    inputStr
     mov     edx, msjP1_2
     call    new_puts
-    mov     edx, cadena
+    mov     edx, StrBin
     call    new_puts
     call    salto
-    ; mov     eax, cadena
-    ; mov     esi, temp
-    ; call    printHex
-    push    ebx
-    mov     ebx,  cadena
-    add     ebx, 5
-    mov     edx, ebx 
-    mov     edi, 5
-    call    outputStr
-    
+
+    ; Despues de aqui.
+    ; hex          6    F   1     8   h
+    ; StrBin -> "0110 1111 0001 1000" b
+
+    mov     edx, StrBin
+    mov     esi, cad        ; Mover una cadena de 8 bytes
+    call    cadBinToHex
+
+    mov     edx, esi
+    call    new_puts
 
 
-
-
-
-    
-
-
-
+;retunr 0
 mov eax, 1
 mov ebx, 0
 int 80h
+
+cadBinToHex:
+    ; Entrada: edx -> dir de la cadena binaria a convertir.
+    ; Salida:  esi -> cad de min 4 bytes para el resultado
+    pusha
+    mov ecx, 4          ; Para 4 dígitos hex
+    mov ebx, 0          ; Acumulador para el valor actual
+    
+.procesar_grupo:
+    mov edi, 4          ; 4 bits por grupo
+    xor bl, bl          ; Limpiar acumulador
+    
+.procesar_bit:
+    dec edi
+    mov al, [edx]       ; Leer siguiente carácter
+    cmp al, '1'
+    jne .no_sumar
+    
+    ; Sumar según posición (1,2,4,8)
+    cmp edi, 0
+    je .sumar_1
+    cmp edi, 1
+    je .sumar_2
+    cmp edi, 2
+    je .sumar_4
+    ; edi = 3
+    add bl, 8
+    jmp .no_sumar
+    
+.sumar_4:
+    add bl, 4
+    jmp .no_sumar
+.sumar_2:
+    add bl, 2
+    jmp .no_sumar
+.sumar_1:
+    add bl, 1
+    
+.no_sumar:
+    inc edx             ; Siguiente bit
+    cmp edi, 0
+    jg .procesar_bit    ; Procesar 4 bits
+    
+    ; Convertir nibble a carácter hex
+    cmp bl, 9
+    jg .letra_hex
+    add bl, '0'
+    jmp .guardar_car
+    
+.letra_hex:
+    sub bl, 10
+    add bl, 'A'
+    
+.guardar_car:
+    mov [esi + ecx - 1], bl
+    loop .procesar_grupo
+    mov byte [esi + 4], 0  ; Terminar cadena
+    popa
+    ret
